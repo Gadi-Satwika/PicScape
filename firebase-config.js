b3013dc2caaae5989348a14913f022dc9af8cdd0
Gadi Satwika <satwikagadi2005@gmail.com>
2025-06-22 12:07:49 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAclT0t-5EVAbJ0IqB_u3XvmpEaNc4arFk",
+    authDomain: "login-page-ace14.firebaseapp.com",
+    projectId: "login-page-ace14",
+    storageBucket: "login-page-ace14.firebasestorage.app",
+    messagingSenderId: "1099176902536",
+    appId: "1:1099176902536:web:0ddf7ce1e77f365506843d",
+};
